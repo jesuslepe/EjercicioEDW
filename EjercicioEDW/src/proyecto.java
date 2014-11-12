6ab599ea68fd051c22cf02fc33ccaeed557af511
usuario1 <usuario1@marisma-54039b6>
2014-11-12 13:54:47 +0100

    Prog1 0

---
+++ EjercicioEDW/src/proyecto.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+import java.lang.Math; 
+
+public class proyecto {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner leerradio = new Scanner(System.in);
+		int radio;
+		System.out.println("Introduce el radio de la esfera");
+		radio=leerradio.nextInt(); 
+	}
+
+}
